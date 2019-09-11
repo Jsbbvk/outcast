4122e60bed1c4be83978dc0c85ea92c8adcb9979
Jsbbvk <Jsbbvk@gmail.com>
2019-09-11 00:21:59 -0700

    small change

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,6 +255,9 @@ function displayAnswerDisplay() {
 }
 
 function displayPQA() {
+    if (infoVisible) {
+        hideInfo();
+    }
     $('#endGameButton').css('display', 'none');
     state = DISPLAYP;
     socket.removeAllListeners('player answered');
@@ -265,7 +268,11 @@ function displayPQA() {
     socket.emit('get room info', roomID, function(rm) {
         $('#playerCategory').text(rm.category);
         $('#playerTopic').text(rm.topic);
-        //$('#playerFakeTopic').text(rm.fakeTopic);
+        if (isOutcast) {
+            $('#playerFakeTopicH').css('display','block');
+            $('#playerFakeTopic').text(rm.fakeTopic);
+        }
+        else $('#playerFakeTopicH').css('display','none');
     });
 
     socket.emit('get players', roomID, function(pl){
@@ -449,26 +456,3 @@ function hideInfo() {
     }
 }
 
-function toggleInfo(state) {
-    if (state=="hidden") {
-        infoVisible = false;
-        document.getElementById("spy-roleS").innerText = (infoVisible)?"Your role: ":"Hidden";
-        document.getElementById("spy-role").innerText = (infoVisible)?spyRole:"";
-        if (spyRole != "Spy" && spyRole!="Leader") document.getElementById("spy-location").innerHTML =
-            (infoVisible)?"Location: <b>"+spyLocation+"</b>": "";
-        if (mode=="leader") document.getElementById("spy-leader").innerHTML = "";
-        if (mode=="movie") document.getElementById("spy-leader").innerHTML = "";
-        document.getElementById("info-container").style.opacity = (infoVisible)?"1":"0.5";
-        document.getElementById("info-help").innerText = (infoVisible)?"Double click to hide":"Double click to show";
-    } else if (state=="visible") {
-        infoVisible = true;
-        document.getElementById("spy-roleS").innerText = (infoVisible)?"Your role: ":"Hidden";
-        document.getElementById("spy-role").innerText = (infoVisible)?spyRole:"";
-        if (spyRole != "Spy" && spyRole!="Leader") document.getElementById("spy-location").innerHTML =
-            (infoVisible)?"Location: <b>"+spyLocation+"</b>": "";
-        if (mode=="leader" && spyRole != "Spy" && spyRole!="Leader") document.getElementById("spy-leader").innerHTML = "Leader: <b>"+spyLeader+"</b>";
-        if (theme=="movie" && spyRole != "Spy" && spyRole!="Leader") document.getElementById("spy-M-ref").innerHTML = (infoVisible)?"Movie: <b>"+getReferenceFromMovie(spyLocation)+"</b>":"";
-        document.getElementById("info-container").style.opacity = (infoVisible)?"1":"0.5";
-        document.getElementById("info-help").innerText = (infoVisible)?"Double click to hide":"Double click to show";
-    }
-}
